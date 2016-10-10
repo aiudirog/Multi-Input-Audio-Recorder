f7c70dcd01ac6a310ae898f01a0bffcfad35b302
aiudirog <aiudirog@gmail.com>
2016-10-09 20:40:58 -0400

    Testing to see if this optimizes anything. It didn t

--- qaudiowaveform.cpp
+++ qaudiowaveform.cpp
@@ -1,5 +1,6 @@
 #include <QPainter>
 #include "qaudiowaveform.h"
+#include <QTime>
 
 #include <QDebug>
 
@@ -35,15 +36,16 @@ void QAudioWaveform::paintEvent(QPaintEvent *event)
     qreal fractionalLevel;
     for (int i = 0; i < m_levels.count(); i++) {
         fractionalLevel = m_levels.at(i);
-        if (fractionalLevel > .7) {
-            paint = QColor(255, 0, 0);      // Red
-        } else if (fractionalLevel > .6) {
-            paint = QColor(255, 144, 0);    // Orange
-        } else if (fractionalLevel > .5) {
-            paint = QColor(255, 212, 0);    // Yellow
-        } else {
+        if (fractionalLevel < .5) {
             paint = QColor(0, 150, 35);     // Green
+        } else if (fractionalLevel < .6) {
+            paint = QColor(255, 212, 0);    // Yellow
+        } else if (fractionalLevel < .7) {
+            paint = QColor(255, 144, 0);    // Orange
+        } else {
+            paint = QColor(255, 0, 0);      // Red
         }
+
         painter.setPen(QPen(paint, 1, Qt::SolidLine, Qt::RoundCap));
         level = fractionalLevel * height();
         if (waveformNumber % 2 == 0)
